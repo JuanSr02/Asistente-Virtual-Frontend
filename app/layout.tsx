007c6b78ad854404c2e81ada7f0ab1e8c87973f0
JuanSr02 <juanma2002123@gmail.com>
2025-06-05 10:29:10 -0300

    Titles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Asistente Virtual',
+  description: 'Asistente para ayudar a los estudiantes',
+  generator: 'Juan Sanchez',
 }
 
 export default function RootLayout({
